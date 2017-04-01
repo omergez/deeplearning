ffd6193f022d2fcea4dc69ffc4fe410a607ee72f
Omer Gez <omer.gez22@gmail.com>
2017-04-01 19:32:13 +0300

    ex3

--- transfer_learning_project.lua
+++ transfer_learning_project.lua
@@ -5,7 +5,7 @@ require 'optim'
 logger = optim.Logger('Transfer.log') -- logger can be changed  
 logger:setNames{'Trainset Error', 'Testset Error'}
 
-local numClasses = 8
+local numClasses = 4
 
 dataset = torch.load('flowers.t7')
 
