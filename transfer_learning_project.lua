afb6a7360dae764cd78e442b6d6b85e7b4996483
Omer Gez <omer.gez22@gmail.com>
2017-04-02 20:57:32 +0300

    ex3

--- transfer_learning_project.lua
+++ transfer_learning_project.lua
@@ -185,7 +185,7 @@ end
    print("Train classes label: ")
    print(trainLabels:narrow(1,1,10))
 
-   local z = model:forward(torch.rand(1,3,128,128):float()):exp():sum()
+   local z = model:forward(torch.rand(1,3,128,128):float()):exp()
    
    print("LogSoftMax:")
    print(z)
